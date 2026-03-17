95920b7d99a6d5d61f71743b2cad504e702f5cb1
abc13379999 <binhnhth03490@fpt.edu.vn>
2026-03-17 18:53:50 +0700

    nah

--- src/Front-End/ADMIN/FrameInterface.vue
+++ src/Front-End/ADMIN/FrameInterface.vue
@@ -2,10 +2,30 @@
 import { useAdminMenu } from "../JS/FrameInterface.JS";
 import CounterOrder from "./CounterOrder.vue";
 import AdminPOS from './AdminPOS.vue'
-import AdminInvoice from'./AdminInvoice.vue'
+import AdminInvoice from './AdminInvoice.vue'
 import Products from "./Products.vue";
 import KhuyenMai from "./KhuyenMai.vue";
-const { menuItems, setActive, isActive, logout, activeIndex } = useAdminMenu();
+
+const { menuItems, activeIndex, openMenu, openSub, logout } = useAdminMenu();
+
+function clickMenu(item) {
+  activeIndex.value = item.id
+  if (item.children) {
+    openMenu.value = openMenu.value === item.id ? null : item.id
+  } else {
+    openMenu.value = null
+  }
+  openSub.value = null
+}
+
+function clickSub(child) {
+  activeIndex.value = child.id
+  if (child.subChildren) {
+    openSub.value = openSub.value === child.id ? null : child.id
+  } else {
+    openSub.value = null
+  }
+}
 </script>
 
 <style scoped src="../CSS/FrameInterface.CSS"></style>
@@ -20,9 +40,45 @@ const { menuItems, setActive, isActive, logout, activeIndex } = useAdminMenu();
 
       <ul class="sidebar-nav">
         <li v-for="item in menuItems" :key="item.id">
-          <button class="nav-item" :class="{ active: isActive(item.id) }" @click="setActive(item.id)">
+
+          <button
+            class="nav-item"
+            :class="{ active: activeIndex === item.id }"
+            @click="clickMenu(item)"
+          >
             {{ item.label }}
+            <span v-if="item.children">{{ openMenu === item.id ? '▴' : '▾' }}</span>
           </button>
+
+          <!-- Level 1 -->
+          <ul v-if="item.children && openMenu === item.id" class="submenu">
+            <li v-for="child in item.children" :key="child.id">
+
+              <button
+                class="nav-item sub-item"
+                :class="{ active: activeIndex === child.id }"
+                @click="clickSub(child)"
+              >
+                {{ child.label }}
+                <span v-if="child.subChildren">{{ openSub === child.id ? '▴' : '▾' }}</span>
+              </button>
+
+              <!-- Level 2 -->
+              <ul v-if="child.subChildren && openSub === child.id" class="submenu level2-menu">
+                <li v-for="sub in child.subChildren" :key="sub.id">
+                  <button
+                    class="nav-item sub-item level2"
+                    :class="{ active: activeIndex === sub.id }"
+                    @click="activeIndex = sub.id"
+                  >
+                    {{ sub.label }}
+                  </button>
+                </li>
+              </ul>
+
+            </li>
+          </ul>
+
         </li>
       </ul>
 
@@ -31,14 +87,12 @@ const { menuItems, setActive, isActive, logout, activeIndex } = useAdminMenu();
       </div>
     </aside>
 
-    <!-- Phần Nội dung code Bên phải  -->
     <main class="main-content">
-      <!-- Link Đơn hàng tại quầy -->
-      <CounterOrder v-if="activeIndex === 2" @openCustom="activeIndex = 10"></CounterOrder>
-      <AdminPOS v-if="activeIndex === 10"></AdminPOS>
-      <Products v-if="activeIndex === 3"></Products>
-      <AdminInvoice v-if="activeIndex === 6"></AdminInvoice>
-      <KhuyenMai v-if="activeIndex === 5"></KhuyenMai>
+      <CounterOrder v-if="activeIndex === 2" @openCustom="activeIndex = 10" />
+      <AdminPOS v-if="activeIndex === 10" />
+      <Products v-if="activeIndex === 31" />
+      <AdminInvoice v-if="activeIndex === 6" />
+      <KhuyenMai v-if="activeIndex === 5" />
     </main>
   </div>
 </template>
