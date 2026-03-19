20b03a8801a8f78998ff14d3ecf82c76a1a34bd7
abc13379999 <binhnhth03490@fpt.edu.vn>
2026-03-19 14:30:02 +0700

    nah

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ import KhuyenMai from './Front-End/ADMIN/KhuyenMai.vue'
 import OrderList from './Front-End/ADMIN/OrderList.vue'
 import Products from './Front-End/ADMIN/Products.vue'
 import AdminEmployee from './Front-End/ADMIN/AdminEmployee.vue'
+import QuanLySPCachThuc from './Front-End/ADMIN/QuanLySPCachThuc.vue'
+import QuanLySPSua from './Front-End/ADMIN/QuanLySPSua.vue'
+import QuanLySPHatCaPhe from './Front-End/ADMIN/QuanLySPHatCaPhe.vue'
+import QuanLySPkem from './Front-End/ADMIN/QuanLySPkem.vue'
+import QuanLySPKemBeo from './Front-End/ADMIN/QuanLySPKemBeo.vue'
 
 const routes = [
     {
@@ -22,6 +27,12 @@ const routes = [
             { path: 'QuanLyDonHang', component: OrderList },
             { path: 'QuanLySanPham', component: Products },
             { path: 'QuanLyNhanVien', component: AdminEmployee },
+
+            { path: 'SanPhamKem', component: QuanLySPkem },
+            { path: 'SanPhamkemBeo', component: QuanLySPKemBeo },
+            { path: 'SanPhamSua', component: QuanLySPSua },
+            { path: 'SanPhamHatCaPhe', component: QuanLySPHatCaPhe },
+            { path: 'QuanLyCachThuc', component: QuanLySPCachThuc },
         ]
     }
 ]
