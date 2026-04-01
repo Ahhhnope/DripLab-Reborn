a484c376da7ace78cee39d08065bb46a6470104f
abc13379999 <binhnhth03490@fpt.edu.vn>
2026-04-01 13:49:18 +0700

    nah

--- src/User/main.js
+++ src/User/main.js
@@ -13,11 +13,8 @@ import UserOrders from '../Front-End/USER/UserOrders.vue'
 import UserStores from '../Front-End/USER/UserStores.vue'
 import MenuView from '../Front-End/USER/MenuView.vue'
 import ProductDetailView from '../Front-End/USER/ProductDetailView.vue'
-<<<<<<< HEAD
 import UserReceipts from '../Front-End/USER/UserReceipts.vue'
 import AboutUS from '../Front-End/USER/AboutUS.vue'
-=======
->>>>>>> 1974f5bc07ae0d19294bcff6c62cc5ac911fae11
 
 //Uy quyen
 import LoginAcc from '../Front-End/Authorization/Login.vue'
