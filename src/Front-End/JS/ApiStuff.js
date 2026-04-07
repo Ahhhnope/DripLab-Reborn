d2f768fbd07aa2a7dd4f093fcec90cd4e7a40bc7
Ahhhnope <magelova32@gmail.com>
2026-04-07 08:46:46 +0700

    cleaning up ApiStuff.js

--- src/Front-End/JS/ApiStuff.js
+++ src/Front-End/JS/ApiStuff.js
@@ -1,8 +1,8 @@
-import api from '../../api'
+import api from '../../api/axios'
 
 
 export async function getDrinks() {
-  const res = await api.get('/drinks').data;
+  const res = await api.get('/drinks');
   return res.data;
 }
 
@@ -19,15 +19,10 @@ export async function addDrink(drink) {
     iceCreamId: drink.iceCreamId ? parseInt(drink.iceCreamId) : null,
     instructionId: drink.instructionId ? parseInt(drink.instructionId) : null,
   });
-
-  if (!res.ok) {
-    const err = await res.text();
-    throw new Error(err);
-  }
 }
 
 export async function updateDrink(drink) {
-  await api.put('/drinks/update/{drink.id}', {
+  await api.put(`/drinks/update/${drink.id}`, {
     name: drink.name,
     category: drink.category,
     basePrice: parseFloat(drink.basePrice),
@@ -42,7 +37,7 @@ export async function updateDrink(drink) {
 }
 
 export async function deleteDrink(id) {
-  await api.delete('/drinks/remove/{id}');
+  await api.delete(`/drinks/remove/${id}`);
 }
 
 
@@ -51,13 +46,9 @@ export async function deleteDrink(id) {
 export async function uploadImage(file) {
   const formData = new FormData();
   formData.append("file", file);
+  const res = await api.post('/upload/image', formData);
 
-  const res = await api.post('/upload/image', {
-    body: formData,
-  });
-  const data = await res.json();
-
-  return data.imageUrl; // trả về "/IMG/uuid.jpg"
+  return res.data.imageUrl; // trả về "/IMG/uuid.jpg"
 }
 
 
@@ -73,7 +64,7 @@ export async function getMilks() {
 }
 
 export async function getHeavyCreams() {
-  return (await api.get('/ingredients/heavy-beans')).data;
+  return (await api.get('/ingredients/heavy-creams')).data;
 }
 
 export async function getIceCreams() {
